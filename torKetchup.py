1b132f49141324a83d7eb5a52bcaa05d369136af
Frankenmint <admin@honeybadgerofmoney.com>
2018-11-27 08:49:24 -0800

    wrap search query in try block in case tbp is down

--- torKetchup.py
+++ torKetchup.py
@@ -8,7 +8,7 @@ import os
 
 def main():
     while True:
-        showString = raw_input("What Show are we catching up on starting with episode # (ex: Family Guy S04E01)\n")
+        showString = raw_input("\nWhat Show are we catching up on starting with episode # (ex: Family Guy S04E01)\n")
         
         if (showString == 'q'): 
            raise SystemExit
@@ -21,19 +21,18 @@ def main():
 
         showSeason = showString.rsplit("E",1)[0]
         start = int(showString.rsplit("E",1)[1])
-
+        browser = webdriver.Chrome(executable_path='%s/chromedriver' % (os.getcwd()) )
         for i in range (start, int(endEpisode)+1):
             lookup = showSeason+"E"+ str(i).zfill(2)
-            browser = webdriver.Chrome(executable_path='%s/chromedriver' % (os.getcwd()) )
-            browser.get('https://thepiratebay.org/')
-            element = browser.find_elements_by_name("q")[1]
-            element.send_keys(lookup)
-            element.submit()
             try:
+                browser.get('https://thepiratebay.org/')
+                element = browser.find_elements_by_xpath('//*[@id="inp"]/input')[0]
+                element.send_keys(lookup)
+                element.submit()
                 magnet = browser.find_elements_by_xpath('//*[@id="searchResult"]/tbody/tr[1]/td[2]/a')[0] #select the first magnet icon
                 # print lookup
-                print magnet.get_attribute('href')
-                #magnet.click() #click the magnet icon href
+                # print magnet.get_attribute('href')
+                magnet.click() #click the magnet icon href
             except IndexError:
                 print "Unable to Find Torrent f0r %s" % (lookup)
 
