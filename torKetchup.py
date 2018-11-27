2fc96df7b996475d1488025c4c007cb3fa343969
Frankenmint <admin@honeybadgerofmoney.com>
2018-11-27 08:35:18 -0800

    try catch error handling missing torrents

--- torKetchup.py
+++ torKetchup.py
@@ -3,6 +3,41 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+import os
 
-browser = webdriver.Chrome(executable_path=r"/Users/frankenmint/Desktop/sideprojects/python/torrentKetchup/chromedriver")
-browser.get('https://automatetheboringstuff.com')
+
+def main():
+    while True:
+        showString = raw_input("What Show are we catching up on starting with episode # (ex: Family Guy S04E01)\n")
+        
+        if (showString == 'q'): 
+           raise SystemExit
+
+        endEpisode = raw_input("Which episode until we're caught up? (ex: 14, for episode 14)\n")
+
+        # showString = "family guy S16E01"
+        # endEpisode = "3"
+        
+
+        showSeason = showString.rsplit("E",1)[0]
+        start = int(showString.rsplit("E",1)[1])
+
+        for i in range (start, int(endEpisode)+1):
+            lookup = showSeason+"E"+ str(i).zfill(2)
+            browser = webdriver.Chrome(executable_path='%s/chromedriver' % (os.getcwd()) )
+            browser.get('https://thepiratebay.org/')
+            element = browser.find_elements_by_name("q")[1]
+            element.send_keys(lookup)
+            element.submit()
+            try:
+                magnet = browser.find_elements_by_xpath('//*[@id="searchResult"]/tbody/tr[1]/td[2]/a')[0] #select the first magnet icon
+                # print lookup
+                print magnet.get_attribute('href')
+                #magnet.click() #click the magnet icon href
+            except IndexError:
+                print "Unable to Find Torrent f0r %s" % (lookup)
+
+        
+
+if __name__ == "__main__":
+    main()
